c9f379776bccd4f7ad6401c4dcdc4dc1824e9ce6
Ej Myles <ejelymyles@gmail.com>
2023-07-14 13:48:05 -0700

    3rd attempt

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { image } from "../data/data";
 function About() {
   return <div id="about">
     <h2>About Me</h2>
-    <p>Sharks are my favorite animal!</p>
+    <p>I made this</p>
     <img src={image} alt="I made this"></img>
   </div>;
 }
